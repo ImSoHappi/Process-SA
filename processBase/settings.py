7411e60689f1d59a1747170bdc63e15b682f9b9b
Samuel Peña guzman <samuelpguzman@hotmail.com>
2020-10-12 14:03:05 -0300

    base de datos amazon

--- processBase/settings.py
+++ processBase/settings.py
@@ -78,11 +78,17 @@ WSGI_APPLICATION = 'processBase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '3.227.211.148:1521/xe',
+        'USER': 'c##processbd',
+        'PASSWORD': '123asd123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

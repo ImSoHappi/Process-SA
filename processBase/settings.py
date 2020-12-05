879dc0257fe59b86500a86aca0a36b2aea138222
Samuel Peña guzman <samuelpguzman@hotmail.com>
2020-12-05 12:54:12 -0300

    manifest

--- processBase/settings.py
+++ processBase/settings.py
@@ -82,10 +82,17 @@ WSGI_APPLICATION = 'processBase.wsgi.application'
 
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '3.236.242.235:1521/xe',
+        'USER': 'c##processbd',
+        'PASSWORD': '123asd123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 # Password validation
